36339142f9bd643223160da877f617253730a722
robertgamer4 <52581711+robertgamer4@users.noreply.github.com>
2020-02-09 17:41:34 -0800

    Complete restructuring. 0.2.0 release.
    Additions
    - Standardizes time and distance
    - Restructures classes
    - Rendering is now separate from updating
    - Main menu
    - Backgrounds
    - Conservation of momentum
    - New Font
    - Many minor improvements

--- physics_engine.py
+++ physics_engine.py
@@ -2,7 +2,7 @@ import pygame as pg
 import sys
 import time
 import random as rm
-from math import sqrt, floor, ceil
+from math import sqrt, ceil
 
 
 def engine():
@@ -10,19 +10,29 @@ def engine():
     # Initialization
     pg.init()
     pg.display.init()
-    SCR_HEIGHT, SCR_WIDTH = 400, 400  # screen
-    WLD_HEIGHT, WLD_WIDTH = 200, 100  # world
-    UNIT = "m"
+    SCR_HEIGHT, SCR_WIDTH = 400, 400  # screen dimensions
+    WLD_HEIGHT, WLD_WIDTH = 200, 200  # earth dimensions
+    SPACE_HEIGHT, SPACE_WIDTH = 400, 400 #space dimensions
+    UNIT = "m" # unit for Y-axis scale
     TICK_MARKS = 5  # number of Y-axis tick marks
-    SCREEN = pg.display.set_mode([SCR_WIDTH, SCR_HEIGHT])
-    SCREEN.fill((255, 255, 255))  # white screen
-    pg.display.set_caption("physics_engine v.0.1")
-    font = pg.font.Font(None, 30)
+    SCREEN = pg.display.set_mode([SCR_WIDTH, SCR_HEIGHT]) # creates screen
+    SCREEN.fill((255, 255, 255))  # blanks screen
+    pg.display.set_caption("physics_engine v.0.2.0")
+    FONT = pg.font.Font("Beckman-Free.otf", 30) # regular font
+    FONTS = pg.font.Font("Beckman-Free.otf", 20) # small font
+    SIMS = ["Earth 01", "Space 01"] # simulation names
+    SHOW_VECTORS = False # displays acceleration and velocity vectors
+    menu = True
+
+    STARS_AMOUNT = 50
+    STARS = []
+    for x in range(STARS_AMOUNT):
+        STARS.append([(rm.randint(0, SCR_WIDTH), rm.randint(0, SCR_HEIGHT)), rm.randint(2, 3)])
     
-    #list of constants
-    gc = 1 #gravitational constant
-    em = 1000 #earth's mass (kg)
-    er = 40 #earth's radius (m)
+    # list of universal constants
+    gc = 0.5 # gravitational constant
+    em = 1000 # earth's mass (kg) 5972370000000000000000000
+    er = 40 # earth's radius (m) 63781000
     
     # QIV -> QI transformations
     def trect(coords):  # transform rectangle (4 inputs)
@@ -38,16 +48,17 @@ def engine():
     class Object():
         instances = []
 
-        def __init__(self, x, y, xv, yv, m, c, xa, ya):
+        def __init__(self, x, y, xv, yv, xa, ya, m, c, e):
             # 'i' prefix = intial
             self.ix = x  # x position
             self.iy = y  # y position
             self.ixv = xv  # x velocity
             self.iyv = yv  # y velocity
+            self.ixa = xa #x acceleration
+            self.iya = ya #y accleration
             self.im = m  # mass
             self.ic = c  # color
-            self.ixa = xa
-            self.iya = ya
+            self.ie = -e # COR
             Object.instances.append(self)
 
         def reset(self):
@@ -55,158 +66,156 @@ def engine():
             self.y = self.iy
             self.xv = self.ixv
             self.yv = self.iyv
-            self.m = self.im
-            self.c = self.ic
             self.xa = self.ixa
             self.ya = self.iya
+            self.m = self.im
+            self.c = self.ic
+            self.e = self.ie
 
-        def update(self):
-            self.xv += self.xa
-            self.yv += self.ya
-            self.y += round(self.yv)
-            self.x += round(self.xv)
+        def update(self, dt):
+            self.xv += self.xa * dt
+            self.yv += self.ya * dt
+            self.x += round(self.xv) * dt
+            self.y += round(self.yv) * dt
+            
+            if SHOW_VECTORS:
+                pg.draw.line(SCREEN,(255,100,0),tcirc((self.x,self.y)),tcirc((self.x+(self.xa*50),self.y+(self.ya*50))),3)                    
+                pg.draw.line(SCREEN,(2,100,100),tcirc((self.x,self.y)),tcirc((self.x+(self.xv*20),self.y+(self.yv*20))),3)
 
         def reset_all():
             for instance in Object.instances:
                 instance.reset()
 
-    class SObject(Object):  # "Space" objects
-        bodies = []
+    class SPhysics():  # Space physics
 
-        def __init__(self, x, y, xv, yv, m, c, xa, ya):
-            super().__init__(x, y, xv, yv, m, c, xa, ya)
-            SObject.bodies.append(self)
-
-        def update(self):
-            for obj in SObject.bodies:
+        def physics(self):
+            for obj in Object.instances:
                 if self.x != obj.x and self.y != obj.y:
-                    #calculates magnitude of acceleration vector
-                    self.am = -1 * (gc * obj.m) / sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2)                    
-                    #calculates acceleration vector
-                    self.xa = self.am*((self.x-obj.x)/sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2))
-                    self.ya = self.am*((self.y-obj.y)/sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2))            
-            super().update()
-
-    class Satellite(SObject):
-        def __init__(self, x, y, xv, yv, m, c, xa, ya, r):
-            super().__init__(x, y, xv, yv, m, c, xa, ya)
-            self.r = round((r*SCR_HEIGHT)/WLD_HEIGHT)  # radius
-            self.b = -0.8
-
-        def update(self):
-            super().update()
-            
-            #make this a function later
-
-            for obj in SObject.bodies:
-                if self.x != obj.x and self.y != obj.y and sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2)) < (self.r + obj.r):
                     
-                    self.p = round(sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2)),3)
-
-                    # calculates the coordinates of the furthest intersecting point for each ball
-                    self.sintx, self.sinty = self.x + ((self.r * (obj.x - self.x))/self.p), self.y + ((self.r * (obj.y - self.y))/self.p)  # self intersection
-                    self.ointx, self.ointy = obj.x + ((obj.r * (self.x - obj.x))/self.p), obj.y + ((obj.r * (self.y - obj.y))/self.p)  # obj intersection
-
-                    # calculates the distance between the intersecting points and creates move values
-                    self.h = sqrt((self.sintx - self.ointx) ** 2 + (self.sinty - self.ointy) ** 2) / 2
-                    self.movex, self.movey = floor((self.h * (self.x-obj.x)) / self.p)-1, floor((self.h * (self.y-obj.y)) / self.p)
-
-                    # moves the objects accordingly
-                    self.x += self.movex
-                    obj.x += self.movex * -1
-                    self.y += self.movey
-                    obj.y += self.movey * -1
-                    self.xv *= self.b
-                    self.yv *= self.b
-            
-            pg.draw.circle(SCREEN, self.c, tcirc((self.x, self.y)), self.r)
-
-    #sat1 = Satellite(200, 380, 0, 0, 5, (0, 0, 0), 0, 0, 5)
-    #sat2 = Satellite(100, 50, 0, 0, 30, (0, 0, 0), 0, 0, 10)
-
-    class EObject(Object):  # "Earth" objects
-        instances = []
-
-        def __init__(self, x, y, xv, yv, m, c, xa, ya):
-            super().__init__(x, y, xv, yv, m, c, xa, ya)
-            EObject.instances.append(self)
-
-        def update(self):
+                    # calculates magnitude of acceleration vector
+                    self.am = -(gc * obj.m) / sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2)                                        
+                    # calculates acceleration vector
+                    if self.m < 30:
+                        self.xa = self.am*((self.x-obj.x)/sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2))
+                        self.ya = self.am*((self.y-obj.y)/sqrt((self.x-obj.x)**2 + (self.y-obj.y)**2))
+        
+        def scl(self, x):
+            return int((x*SCR_HEIGHT)/SPACE_HEIGHT)            
+                         
+    class EPhysics():  # Earth physics
+        def physics(self):
             self.xv += round((rm.random()*2)-1, 2)
-            super().update()
+            self.ya = -(gc*em)/(er**2)
+        
+        def scl(self, x):
+            return int((x*SCR_HEIGHT)/WLD_HEIGHT)
 
-        def reset(self):
-            super().reset()
-            self.ya = -(gc*em)/(er**2)  # gravity setting -0.5
+    class Sphere(Object):
+        def __init__(self, x, y, xv, yv, xa, ya, m, c, e, r):
+            self.r = r
+            super().__init__(x, y, xv, yv, xa, ya, m, c, e)
 
-    class Ball(EObject):
-        def __init__(self, x, y, xv, yv, m, c, xa, ya, r, b):
-            super().__init__(x, y, xv, yv, m, c, xa, ya)
-            self.r = round((r*SCR_HEIGHT)/WLD_HEIGHT)  # radius
-            self.b = -1 * b #bounciness
-
-        def update(self):
-            super().update()
+        def update(self, dt):
+            super().physics()
+            super().update(dt)
 
             # object to object collisions
-            for obj in EObject.instances:
+            for obj in Object.instances:
+                
                 # if the balls are intersecting
                 if self.x != obj.x and self.y != obj.y and ceil(sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2))) <= (self.r + obj.r):
                     
-                    #print(round(sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2)) - (self.r+obj.r)), " before ",end='')
-
                     # calculates distance between centers of balls
-                    self.p = round(sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2)),3)
+                    self.p = round(sqrt((((self.x-obj.x)**2) + ((self.y-obj.y)**2))), 5)
 
                     # calculates the coordinates of the furthest intersecting point for each ball
                     self.sintx, self.sinty = self.x + ((self.r * (obj.x - self.x))/self.p), self.y + ((self.r * (obj.y - self.y))/self.p)  # self intersection
                     self.ointx, self.ointy = obj.x + ((obj.r * (self.x - obj.x))/self.p), obj.y + ((obj.r * (self.y - obj.y))/self.p)  # obj intersection
 
                     # calculates the distance between the intersecting points and creates move values
-                    self.h = sqrt((self.sintx - self.ointx) ** 2 + (self.sinty - self.ointy) ** 2) / 2
+                    self.h = round(sqrt(((self.sintx - self.ointx) ** 2) + ((self.sinty - self.ointy) ** 2)) / 2, 5)
                     self.movex, self.movey = ceil((self.h * (self.x-obj.x)) / self.p), ceil((self.h * (self.y-obj.y)) / self.p)
 
-                    # moves the objects accordingly
+                    # moves the objects accordingly, resolving the collision
                     self.x += self.movex
-                    obj.x += self.movex * -1
+                    obj.x -= self.movex
                     self.y += self.movey
-                    obj.y += self.movey * -1
-                    self.xv *= self.b
-                    self.yv *= self.b
+                    obj.y -= self.movey
                     
-                    #print(round(sqrt(((self.x-obj.x)**2 + (self.y-obj.y)**2)) - (self.r+obj.r))," after")
-
-            # side wall collisions
+                    self.xvo, self.yvo = self.xv, self.xv # saves original values
+                    
+                    self.colCOR = 0.9 #(self.e + obj.e) / 2 # collision coefficient of restitution
+                    self.xv = ((self.colCOR * obj.m) * (obj.xv-self.xv) + (self.m * self.xv) + (obj.m * obj.xv)) / (self.m + obj.m)
+                    self.yv = ((self.colCOR * obj.m) * (obj.yv-self.yv) + (self.m * self.yv) + (obj.m * obj.yv)) / (self.m + obj.m)
+                    obj.xv = ((self.colCOR * self.m) * (self.xvo-obj.xv) + (self.m * self.xvo) + (obj.m * obj.xv)) / (self.m + obj.m)
+                    obj.yv = ((self.colCOR * self.m) * (self.yvo-obj.yv) + (self.m * self.yvo) + (obj.m * obj.yv)) / (self.m + obj.m)
+        
+        def render(self):
+            pg.draw.circle(SCREEN, self.c, tcirc((super().scl(self.x), super().scl(self.y))), super().scl(self.r))
+    
+    class Ball(Sphere, EPhysics):
+        def update(self, dt):
+            super().update(dt)
+            
+            # wall collisions
             if self.x <= self.r:
                 self.x = self.r
-                self.xv *= self.b
-            elif self.x >= (SCR_WIDTH-self.r):
-                self.x = SCR_WIDTH-self.r
-                self.xv *= self.b
+                self.xv *= self.e
+            elif self.x >= (WLD_WIDTH-self.r):
+                self.x = WLD_WIDTH-self.r
+                self.xv *= self.e
 
             # ground collisons
             if self.y <= self.r:
                 self.y = self.r
-                self.yv *= self.b
-
-            pg.draw.circle(SCREEN, self.c, tcirc((self.x, self.y)), self.r)
-
-    class Cube(EObject):
-        def __init__(self, x, y, xv, v, m, c, xd, yd):
-            super().__init__(x, y, xv, v, m)
-            self.xd = xd  # x dim
-            self.yd = yd  # y dim
-
-        def update(self):
-            super().update()
-
-    ball_1 = Ball(200, 390, 0, 0, 0, (0, 255, 0), 0, 0, 10, 0.6)
-    ball_2 = Ball(350, 390, 0, 0, 0, (0, 0, 255), 0, 0, 5, 0.8)
-
-    Object.reset_all()
-
+                self.yv *= self.e
+        
+    class Satellite(Sphere, SPhysics):
+        #self.yv = sqrt((gc*self.m)/self.r) - circular orbit? return to
+        pass
+    
+    while menu:
+        
+        SCREEN.blit(FONT.render("Choose simulation", True, pg.Color('black')), trect((int((SCR_WIDTH/2)-(FONT.size("Choose simulation")[0]/2)),SCR_HEIGHT-80,0,0)))
+        buttons = []
+        for x in range(len(SIMS)):
+            buttons.append(pg.draw.rect(SCREEN,(0,0,0),((SCR_WIDTH/2-75,SCR_HEIGHT-200-(x*50),150,30))))
+            SCREEN.blit(FONTS.render(SIMS[x], True, (0,200,0)), (int((SCR_WIDTH/2)-(FONTS.size(SIMS[x])[0]/2)),SCR_HEIGHT-198-(x*50),150,30))
+        keys = pg.key.get_pressed()
+       
+        for event in pg.event.get():
+            if event.type == pg.QUIT or keys[pg.K_q]:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.MOUSEBUTTONDOWN:
+                clicked = [c for c in buttons if c.collidepoint(pg.mouse.get_pos())]
+                
+                if len(clicked) == 1:
+                    sel = int((clicked[0][1]-150)/50) #selected
+                
+                    if sel == 0:
+                        #implement camera later
+                        asteroid = Satellite(20, 200, 0, 1, 0, 0, 5, (61, 61, 41), 0.5, 3)
+                        sun = Satellite(200, 200, 0, 0, 0, 0, 50, (255, 204, 0), 0.5, 10)
+                        planet = Satellite(200, 350, 3, 0, 0, 0, 25, (255, 0, 0), 0.5, 5)
+                    
+                    if sel == 1:
+                        ball_1 = Ball(100, 190, 0, 0, 0, 0, 10, (0, 255, 0), 0.7, 10)
+                        ball_2 = Ball(150, 190, 0, 0, 0, 0, 5, (0, 0, 255), 0.8, 5)
+                        ball_3 = Ball(50, 190, 0, 0, 0, 0, 3, (255, 0, 0), 0.9, 3)
+                        ball_4 = Ball(190, 190, 0, 0, 0, 0, 13, (100, 50, 0), 0.6, 13)
+                    
+                    SIM = SIMS[sel]                    
+                    menu = False
+        
+        pg.display.update()
+    
+    Object.reset_all()    
+    elapsed = time.time()
+   
     # main loop
     while True:
+        SCREEN.fill((255, 255, 255))
 
         # enables quitting by pressing q or clicking red x
         keys = pg.key.get_pressed()
@@ -219,24 +228,37 @@ def engine():
         if keys[pg.K_r]:
             Object.reset_all()
 
+        if SIM == SIMS[0]:
+            SCREEN.fill((0, 0, 0))
+            for x in range(STARS_AMOUNT):
+                pg.draw.circle(SCREEN, (255,255,255), STARS[x][0], STARS[x][1])
+
+        if SIM == SIMS[1]:
+            # builds Y-axis scale
+            for x in range(TICK_MARKS):
+                a = x*round(SCR_HEIGHT/TICK_MARKS)
+                if x % 2 == 0:
+                    pg.draw.rect(SCREEN, (240,240,240), trect([0, a, SCR_WIDTH, SCR_HEIGHT/TICK_MARKS]))
+                rect((0, a, 5, 2))
+                SCREEN.blit(FONTS.render(str(int((x*WLD_HEIGHT)/TICK_MARKS)) + UNIT, True, pg.Color('black')), trect((2, a+13, 10, a)))
+        
+        dt = (time.time() - elapsed) * 50
+        elapsed = time.time()
+        
         if keys[pg.K_s]:
-            time.sleep(0.1)
+            dt /= 50
 
         if keys[pg.K_d]:
-            time.sleep(0.25)
+            dt /= 100
 
-        # builds Y-axis scale
-        for x in range(TICK_MARKS):
-            a = x*round(SCR_HEIGHT/TICK_MARKS)
-            rect((0, a, 5, 2))
-            SCREEN.blit(font.render(str(int((x*WLD_HEIGHT)/TICK_MARKS)) + UNIT, True, pg.Color('black')), trect((2, a+10, 10, a+5)))
-        
         for instance in Object.instances:
-            instance.update()
+            instance.update(dt)
+
+        for instance in Object.instances:
+            instance.render()
 
         # updates frame and resets
         pg.display.update()
-        SCREEN.fill((255, 255, 255))
 
 
 engine()
